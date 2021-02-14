87a083d377af411c4228985a24bfa490d10831c6
shrijanapoudel <shrijanapoudel@whitehatengineering.com>
2021-02-14 13:47:08 +0545

    Docker files

--- backend2/notification.js
+++ backend2/notification.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+require('dotenv');
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
